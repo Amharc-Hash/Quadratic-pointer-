8012fd1750e4e5ad6f855ea1e82c14b233b748e0
VACHARA VIRIYAKUL <63010871@KMITL.AC.TH>
2020-08-30 21:50:05 +0700

    add array

--- Quadratic(pointer)/Quadratic.cpp
+++ Quadratic(pointer)/Quadratic.cpp
@@ -9,18 +9,21 @@ void solve(double a, double b, double c, double* x1, double* x2) {
 
 int main()
 {
-	double a, b, c;
+	double k[1000];
 	double x1, x2;
-	printf("Please follow this pattern to add numbers \n ex) x^2 + 2x + 1 = 1 2 1 \n your number is :  ");
-	scanf_s("%lf %lf %lf", &a, &b, &c);
-	if (pow(b, 2) - (4 * a * c) > 0 && a != 0)
+	printf("Please follow this pattern to add numbers \n ex) x^2 + 2x + 1 = \n1\n2\n1 \n your number is :\n");
+	for (int i = 0; i < 3; i++)
 	{
-		solve(a, b, c, &x1, &x2);
+		scanf_s("%lf", &k[i]);
+	}
+	if (pow(k[1], 2) - (4 * k[0] * k[2]) > 0 && k[0] != 0)
+	{
+		solve(k[0], k[1], k[2], &x1, &x2);
 		printf("x1 = %.2lf x2 = %.2lf\n\n", x1, x2);
 	}
-	else if (pow(b, 2) - (4 * a * c) == 0 && a != 0)
+	else if (pow(k[1], 2) - (4 * k[0] * k[2]) == 0 && k[0] != 0)
 	{
-		printf("x = %.2lf\n\n", -b / (2.0 * a));
+		printf("x = %.2lf\n\n", k[1] / (2.0 * k[0]));
 	}
 	else
 	{
